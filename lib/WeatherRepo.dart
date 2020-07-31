f109b0ae1022acf802f787591fc205c3c1a0251b
mjstokes86 <mjstokes1986@att.net>
2020-07-31 06:31:00 -0400

    deleted api key

--- lib/WeatherRepo.dart
+++ lib/WeatherRepo.dart
@@ -6,7 +6,7 @@ import 'package:weather_app/WeatherModel.dart';
 class WeatherRepo {
   Future<WeatherModel> getWeather(String city) async {
     final result = await http.Client().get(
-        "http://api.openweathermap.org/data/2.5/weather?q=$city&appid=6d423e228a6758dc11527ada6ad2c1d5");
+        "http://api.openweathermap.org/data/2.5/weather?q=$city&appid={apikeygoeshere}");
 
     if (result.statusCode != 200) throw Exception();
     return parsedJson(result.body);
